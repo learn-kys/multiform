95e77ae5258bbe034d82ff9544cb379363b4f8c1
KYs <amitkys59@gmail.com>
2025-10-01 13:09:14 +0530

    init prisma

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,52 +1,8 @@
-// "use client";
+import prisma from "@/lib/db";
+export default async function Page() {
+  const user = await prisma.user.findFirst({
+    where: { name: "John Doe" },
+  });
 
-// import * as React from "react";
-// import { CalendarIcon } from "lucide-react";
-// import { format } from "date-fns";
-
-// import { Button } from "@/components/ui/button";
-// import { Calendar } from "@/components/ui/calendar";
-// import {
-//   Popover,
-//   PopoverContent,
-//   PopoverTrigger,
-// } from "@/components/ui/popover";
-// import { cn } from "@/lib/utils";
-
-// export default function SampleDatePicker() {
-//   const [date, setDate] = React.useState<Date>();
-
-//   return (
-//     <Popover>
-//       <PopoverTrigger asChild>
-//         <Button
-//           className={cn(
-//             "w-[240px] justify-start text-left font-normal",
-//             !date && "text-muted-foreground",
-//           )}
-//           variant={"outline"}
-//         >
-//           <CalendarIcon className="mr-2 h-4 w-4" />
-//           {date ? format(date, "dd/MM/yyyy") : <span>Pick a date</span>}
-//         </Button>
-//       </PopoverTrigger>
-//       <PopoverContent align="start" className="w-auto p-0">
-//         <Calendar
-//           captionLayout="dropdown"
-//           disabled={{
-//             after: new Date(),
-//             before: new Date(1900, 0),
-//           }}
-//           endMonth={new Date()}
-//           mode="single"
-//           selected={date}
-//           onSelect={setDate}
-//         />
-//       </PopoverContent>
-//     </Popover>
-//   );
-// }
-
-export default function Page() {
-  return <input type="date" />;
+  return <div>{user?.name}</div>;
 }
