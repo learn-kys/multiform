321bb87fe42b61ac94030b4dd8a6503004f39fba
KYs <amitkys59@gmail.com>
2025-10-10 20:34:35 +0530

    improved ui

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -110,7 +110,7 @@ export default function Page() {
               >
                 {form.formState.isSubmitting ? (
                   <>
-                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                    <Loader2 className=" h-4 w-4 animate-spin" />
                     Signing In...
                   </>
                 ) : (
