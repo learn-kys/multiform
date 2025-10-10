f7fd1db8a3fa4ab9edeb71459680ee8963c9a40e
KYs <amitkys59@gmail.com>
2025-10-10 20:19:35 +0530

    main page where showing user temp credentails

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { toTitleCase } from "@/lib/utils";
 
 export default function Page() {
   const { data: session, isPending: loading } = authClient.useSession();
@@ -12,12 +13,20 @@ export default function Page() {
     <div>
       {session == null ? (
         <Button>
-          <Link href={"/auth/login"}>SignIn / Sign Up</Link>
+          <Link href={"/signin"}>SignIn / Sign Up</Link>
         </Button>
       ) : (
         <>
-          <h1>welcome {session.user.name.toLocaleLowerCase()}</h1>
-          <Button onClick={() => authClient.signOut()}>Sign out</Button>
+          <h1>welcome {toTitleCase(session.user.name)}</h1>
+          <p>email {session.user.email} </p>
+          <p>password:AT#I6N63uFr3</p>
+          <Button
+            size={"sm"}
+            variant={"destructive"}
+            onClick={() => authClient.signOut()}
+          >
+            Sign out
+          </Button>
         </>
       )}
     </div>
