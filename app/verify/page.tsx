4dec636dc800358078acf3d1746f926881dcb24c
KYs <amitkys59@gmail.com>
2025-10-11 11:04:35 +0530

    building verfication page

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -1,4 +1,21 @@
 "use client";
+
+import { BetterAuthActionButton } from "@/components/better-auth-action-button";
+import { authClient } from "@/lib/auth-client";
+
 export default function Page() {
-  return null;
+  return (
+    <div>
+      <BetterAuthActionButton
+        action={() => {
+          return authClient.sendVerificationEmail({ email, callbackURL: "/" });
+        }}
+        className="w-full"
+        successMessage="Verification email sent!"
+        variant={"outline"}
+      >
+        Resend Verification Email
+      </BetterAuthActionButton>
+    </div>
+  );
 }
