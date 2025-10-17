020da27a7eb2341c72138da9c75686c9b5cf1932
KYs <amitkys59@gmail.com>
2025-10-17 21:35:49 +0530

    message to error (cause error diprecated

--- app/(form)/personal/_components/personal-form.tsx
+++ app/(form)/personal/_components/personal-form.tsx
@@ -2,8 +2,8 @@ import type { auth } from "@/lib/auth";
 
 // Automatically infers all your custom fields (ref to ~/prg/src/advance-type-extraction.ts) file
 export type AuthSession = Awaited<ReturnType<typeof auth.api.getSession>>;
-export type AuthUser = NonNullable<AuthSession>["user"];
+export type AuthUser = NonNullable<AuthSession>["user"]; // it simply remove null
 
 export default function PersonalForm({ user }: { user: AuthUser }) {
-  return <div>{user.jobTitle}</div>;
+  return <div>{user.dateOfBirth.toDateString()}</div>;
 }
--- lib/types.ts
+++ lib/types.ts
@@ -4,16 +4,16 @@ export const formSchema = z.object({
   firstName: z
     .string()
     .trim()
-    .min(1, { message: "First name is required" })
-    .min(3, { message: "First name is too short" })
-    .max(20, { message: "First name is too long" })
-    .regex(/^[A-Za-z ]+$/, { message: "First name can only contain letters" })
+    .min(1, { error: "First name is required" })
+    .min(3, { error: "First name is too short" })
+    .max(20, { error: "First name is too long" })
+    .regex(/^[A-Za-z ]+$/, { error: "First name can only contain letters" })
     .toUpperCase(),
 
   middleName: z
     .string()
     .trim()
-    .regex(/^[A-Za-z ]*$/, { message: "Middle name can only contain letters" })
+    .regex(/^[A-Za-z ]*$/, { error: "Middle name can only contain letters" })
     .toUpperCase()
     .transform((val) => (val === "" ? null : val))
     .nullable(),
@@ -21,77 +21,144 @@ export const formSchema = z.object({
   lastName: z
     .string()
     .trim()
-    .min(1, { message: "Last name is required" })
-    .max(20, { message: "Last name is too long" })
-    .regex(/^[A-Za-z ]+$/, { message: "Last name can only contain letters" })
+    .min(1, { error: "Last name is required" })
+    .max(20, { error: "Last name is too long" })
+    .regex(/^[A-Za-z ]+$/, { error: "Last name can only contain letters" })
     .toUpperCase(),
 
   fatherFullName: z
     .string()
     .trim()
-    .min(1, { message: "Father's name is required" })
-    .min(3, { message: "Father's name is too short" })
-    .max(40, { message: "Father's name is too long" })
+    .min(1, { error: "Father's name is required" })
+    .min(3, { error: "Father's name is too short" })
+    .max(40, { error: "Father's name is too long" })
     .regex(/^[A-Za-z ]+$/, {
-      message: "Father's name can only contain letters",
+      error: "Father's name can only contain letters",
     })
     .toUpperCase(),
 
   dateOfBirth: z
     .string()
-    .min(1, { message: "Date of birth is required" })
+    .min(1, { error: "Date of birth is required" })
     .refine((val) => !isNaN(Date.parse(val)), {
-      message: "Invalid date format",
+      error: "Invalid date format",
     })
     .refine((val) => new Date(val) <= new Date(), {
-      message: "Date of birth cannot be in the future",
+      error: "Date of birth cannot be in the future",
     }),
 
   phoneNumber: z
     .string()
     .trim()
-    .min(1, { message: "Phone number is required" })
-    .regex(/^[6-9]\d{9}$/, { message: "Invalid phone number" }),
+    .min(1, { error: "Phone number is required" })
+    .regex(/^[6-9]\d{9}$/, { error: "Invalid phone number" }),
 
-  email: z.email({ message: "Invalid email address" }).trim().toLowerCase(),
+  email: z.email({ error: "Invalid email address" }).trim().toLowerCase(),
 
   jobTitle: z
     .string()
     .trim()
-    .min(1, { message: "Job title is required" })
-    .max(50, { message: "Job title must be at most 50 characters long" }),
+    .min(1, { error: "Job title is required" })
+    .max(50, { error: "Job title must be at most 50 characters long" }),
 
-  jobId: z.string().trim().min(1, { message: "Job ID is required" }),
+  jobId: z.string().trim().min(1, { error: "Job ID is required" }),
 
   password: z
     .string()
-    .min(1, { message: "Password is required" })
-    .min(8, { message: "Password too short" }),
+    .min(1, { error: "Password is required" })
+    .min(8, { error: "Password too short" }),
 });
 
 export const signInSchema = z.object({
-  email: z.email({ message: "Invalid email address" }).trim().toLowerCase(),
-  password: z.string().min(1, { message: "Password is required" }).min(8, {
-    message: "Password too short",
+  email: z.email({ error: "Invalid email address" }).trim().toLowerCase(),
+  password: z.string().min(1, { error: "Password is required" }).min(8, {
+    error: "Password too short",
   }),
 });
 
 export const forgetPasswordSchema = z.object({
-  email: z.email({ message: "Invalid email address" }).trim().toLowerCase(),
+  email: z.email({ error: "Invalid email address" }).trim().toLowerCase(),
 });
 
 export const resetPasswordSchema = z
   .object({
     password: z
       .string()
-      .min(1, { message: "Password is required" })
-      .min(8, { message: "Password must be at least 8 characters" }),
+      .min(1, { error: "Password is required" })
+      .min(8, { error: "Password must be at least 8 characters" }),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
+    error: "Passwords do not match",
     path: ["confirmPassword"],
   });
+
+export const CandidatePersonalSchema = z.object({
+  firstName: z
+    .string()
+    .trim()
+    .min(1, { error: "First name is required" })
+    .min(3, { error: "First name is too short" })
+    .max(20, { error: "First name is too long" })
+    .regex(/^[A-Za-z ]+$/, { error: "First name can only contain letters" })
+    .toUpperCase(),
+
+  middleName: z
+    .string()
+    .trim()
+    .regex(/^[A-Za-z ]*$/, { error: "Middle name can only contain letters" })
+    .toUpperCase()
+    .transform((val) => (val === "" ? null : val))
+    .nullable(),
+
+  lastName: z
+    .string()
+    .trim()
+    .min(1, { error: "Last name is required" })
+    .max(20, { error: "Last name is too long" })
+    .regex(/^[A-Za-z ]+$/, { error: "Last name can only contain letters" })
+    .toUpperCase(),
+
+  fatherFullName: z
+    .string()
+    .trim()
+    .min(1, { error: "Father's name is required" })
+    .min(3, { error: "Father's name is too short" })
+    .max(40, { error: "Father's name is too long" })
+    .regex(/^[A-Za-z ]+$/, {
+      error: "Father's name can only contain letters",
+    })
+    .toUpperCase(),
+
+  motherFullName: z
+    .string()
+    .trim()
+    .min(1, { error: "Mother's name is required" })
+    .min(3, { error: "Mother's name is too short" })
+    .max(40, { error: "Mother's name is too long" })
+    .regex(/^[A-Za-z ]+$/, {
+      error: "Mother's name can only contain letters",
+    })
+    .toUpperCase(),
+
+  dateOfBirth: z
+    .string()
+    .min(1, { error: "Date of birth is required" })
+    .refine((val) => !isNaN(Date.parse(val)), {
+      error: "Invalid date format",
+    })
+    .refine((val) => new Date(val) <= new Date(), {
+      error: "Date of birth cannot be in the future",
+    }),
+
+  gender: z.enum(["Male", "Female", "Other"], {
+    error: "Please select a valid gender.",
+  }),
+
+  isMarried: z.boolean({
+    error: "Marital status is required",
+  }),
+});
 
 export type ResetPasswordSchema = z.infer<typeof resetPasswordSchema>;
 export type ForgetPasswordSchema = z.infer<typeof forgetPasswordSchema>;
