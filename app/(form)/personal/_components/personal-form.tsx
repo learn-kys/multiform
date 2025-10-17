ea09c1bb8c15f99d08f84724ab70aacc96d9c79d
KYs <amitkys59@gmail.com>
2025-10-17 21:09:57 +0530

    type ref

--- app/(form)/personal/_components/personal-form.tsx
+++ app/(form)/personal/_components/personal-form.tsx
@@ -1,6 +1,6 @@
 import type { auth } from "@/lib/auth";
 
-// Automatically infers all your custom fields
+// Automatically infers all your custom fields (ref to ~/prg/src/advance-type-extraction.ts) file
 export type AuthSession = Awaited<ReturnType<typeof auth.api.getSession>>;
 export type AuthUser = NonNullable<AuthSession>["user"];
 
